2a93d73d172e96b77857e2cbedd89eb461572a09
lucianoruggeri <lucianoruggeri5@gmail.com>
2024-06-27 04:06:06 -0300

    primeras paginas y ruteo

--- preguntados/src/App.jsx
+++ preguntados/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./screens/home/Home";
+import Game from "./screens/game/Game";
+import Winner from "./screens/winner/Winner";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jugadores, setJugadores] = useState(1); // 1 o 2 jugadores
+  const [nivelDificultad, setNivelDificultad] = useState("facil"); // 'facil', 'medio', 'dificil'
+  const [preguntas, setPreguntas] = useState([]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="background">
+        <div className="modal">
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/play" element={<Game />}></Route>
+              <Route path="/winner" element={<Winner />}></Route>
+            </Routes>
+          </Router>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ preguntados/src/screens/winner/Winner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Winner = () => {
+  return (
+    <div>
+      <p>Winner PAGE</p>
+    </div>
+  );
+};
+
+export default Winner;
