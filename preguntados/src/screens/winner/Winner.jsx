7d64f26548eab30eb905b2bef79edec01759bb4b
lucianoruggeri <lucianoruggeri5@gmail.com>
2024-07-07 21:34:01 -0300

    winner logic

--- preguntados/src/screens/winner/Winner.jsx
+++ preguntados/src/screens/winner/Winner.jsx
@@ -6,20 +6,52 @@ const WinnerComponent = () => {
   const navigate = useNavigate();
   const { correctAnswers, players } = location.state || {};
 
-  const winner = players[0].name;
+  let playersWithScores = players.map((player, index) => ({
+    name: player.name,
+    correctAnswers: correctAnswers[index],
+  }));
+
+  playersWithScores.sort(
+    (player1, player2) => player2.correctAnswers - player1.correctAnswers,
+  );
+
+  let winner;
+
+  if (correctAnswers.length === 1) {
+    winner = players[0].name;
+  } else if (
+    playersWithScores[0].correctAnswers > playersWithScores[1].correctAnswers
+  ) {
+    winner = playersWithScores[0].name;
+  } else if (
+    playersWithScores[0].correctAnswers < playersWithScores[1].correctAnswers
+  ) {
+    winner = playersWithScores[1].name;
+  } else {
+    winner = null; // Empate
+  }
 
   const resetGame = () => {
     navigate("/");
   };
 
   return (
-    <div>
-      <h1>Ganador: {winner}</h1>
-      <p>
-        Respuestas Correctas:{" "}
-        {correctAnswers.length === 0 ? 0 : correctAnswers[0]}
-      </p>
-      <button onClick={resetGame}>PLAY AGAIN!</button>
+    <div className="container">
+      <div className="header">
+        {winner ? <h1>{winner}</h1> : <h1>It's a draw</h1>}
+      </div>
+      <div className="winners">
+        <ul>
+          {playersWithScores.map((player, index) => (
+            <li key={index}>
+              {player.name}: {player.correctAnswers} correct answers
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="buttons">
+        <button onClick={resetGame}>PLAY AGAIN!</button>
+      </div>
     </div>
   );
 };
